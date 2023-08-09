4ae595a9009e231e4a813ae10a0de89c80406f61
ruoyushang <r.y.shang@gmail.com>
2023-08-09 12:02:07 -0700

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,24 @@
 
 source /home/rshang/EventDisplay/root_ubuntu20/root/bin/thisroot.sh
-export SMI_INPUT=/gamma_raid/userspace/rshang/analysis/Results/v487
+
+#export SMI_INPUT=/gamma_raid/userspace/rshang/analysis/Results/v487
+#export SMI_RUNLIST=/home/rshang/NuclearNormMinimizationMethod/runlist_v487
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_nuclear_v487
+
+export SMI_INPUT=/gamma_raid/userspace/rshang/analysis/Results/v490
+export SMI_RUNLIST=/home/rshang/NuclearNormMinimizationMethod/runlist
+export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_nuclear_test
+
 export SMI_AUX=/gamma_raid/userspace/rshang/SMI_AUX
 export SMI_DIR=/home/rshang/NuclearNormMinimizationMethod
 
 
-export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_nuclear
+
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_weight_log_m0p0
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_weight_log_m0p5
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_weight_log_m1p0
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_weight_log_m1p5
+#export SMI_OUTPUT=/gamma_raid/userspace/rshang/SMI_output/output_weight_log_m2p0
 
 export SMI_BIN=$SMI_OUTPUT/bin
-
 
